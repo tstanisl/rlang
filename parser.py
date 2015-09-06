0820be7e123648a14ba85857288725ca07d7b4f1
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2015-09-06 16:12:49 +0200

    parser: add support for equivalence, induction, and, or expressions

--- parser.py
+++ parser.py
@@ -37,7 +37,12 @@ def prepare_grammar():
 	bin_digit = pp.Combine(pp.Suppress('0b') + pp.Word('01'), adjacent = True)
 	digit = dec_digit ^ hex_digit ^ bin_digit
 
-	equiv_expr = digit
+	cmp_expr = digit
+	and_expr = cmp_expr + pp.ZeroOrMore(pp.Suppress('&&') + cmp_expr)
+	or_expr = and_expr + pp.ZeroOrMore(pp.Suppress('||') + and_expr)
+	induc_expr = pp.Forward()
+	induc_expr << or_expr + pp.Optional(pp.Suppress('==>') + induc_expr)
+	equiv_expr = induc_expr + pp.Optional(pp.Suppress('<==>') + induc_expr)
 	cond_expr = equiv_expr + pp.Optional(pp.Suppress('?') + equiv_expr + pp.Suppress(':') + equiv_expr)
 	expr = cond_expr
 
